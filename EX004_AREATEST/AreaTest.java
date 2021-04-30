be9ff0c1999ec9ada6a5aa76a12d8c205665b140
ファン・デヨン <77083563+KRHDY@users.noreply.github.com>
2021-04-30 13:39:56 +0900

    Update AreaTest.java

--- EX004_AREATEST/AreaTest.java
+++ EX004_AREATEST/AreaTest.java
@@ -8,7 +8,7 @@ public class AreaTest {
 			
 			radius	=	5.0;
 			area	=	3.141592 * radius * radius;
-			System.out.println("¿øÀÇ ¸éÀûÀº "+ area);
+			System.out.println("ì›ì£¼ìœ¨ì€"+ area);
 	}
 
 }
